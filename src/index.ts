ee303332c014dba50c22c24e9798765f2a275238
nariman najafi <narimannajafi@narimans-MacBook-Air.local>
2023-10-03 18:58:15 +0330

    sort words

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { Sorter } from './Sorter';
 import { NumberCollection } from './NumberCollection';
+import { CharacterCollection } from './CharacterCollection';
 
-const numberCollection = new NumberCollection([90000, 3, -5, 0]);
-const sorter = new Sorter(numberCollection);
+// const numberCollection = new NumberCollection([90000, 3, -5, 0]);
+// const sorter = new Sorter(numberCollection);
+// sorter.sort();
+// console.log(numberCollection.data);
+
+const characterCollection = new CharacterCollection('Xaayb');
+const sorter = new Sorter(characterCollection);
 sorter.sort();
-console.log(numberCollection.data);
+console.log(characterCollection.data);
