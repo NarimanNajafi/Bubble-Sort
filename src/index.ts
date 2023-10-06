bba3089345f4e6f76c0022917a55f281ca092dd7
nariman najafi <narimannajafi@narimans-MacBook-Air.local>
2023-10-06 11:35:54 +0330

    index updated(after added linkedList

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,25 @@
 import { Sorter } from './Sorter';
 import { NumberCollection } from './NumberCollection';
 import { CharacterCollection } from './CharacterCollection';
+import { LinkedList } from './LinkedList';
 
 // const numberCollection = new NumberCollection([90000, 3, -5, 0]);
 // const sorter = new Sorter(numberCollection);
 // sorter.sort();
 // console.log(numberCollection.data);
 
-const characterCollection = new CharacterCollection('Xaayb');
-const sorter = new Sorter(characterCollection);
+// const characterCollection = new CharacterCollection('Xaayb');
+// const sorter = new Sorter(characterCollection);
+// sorter.sort();
+// console.log(characterCollection.data);
+
+const linkedList = new LinkedList();
+
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log(characterCollection.data);
+linkedList.print();
